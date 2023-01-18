0adf664b5aed89a7f7c0354a4eedcc924df272f7
Ivan Monge <ivanmongebadilla@gmail.com>
2023-01-17 18:54:17 -0800

    Forms

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -15,6 +15,7 @@ import { fetchPartners } from '../features/partners/partnersSlice';
 import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
+import ReservationScreen from './ReservationScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -121,6 +122,29 @@ const ContactNavigator = () => {
   );
 };
 
+const ReservationNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Contact"
+        component={ReservationScreen}
+        options={({ navigation }) => ({
+          title: 'Reservation Search',
+          headerLeft: () => (
+            <Icon 
+              name='tree'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          )
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const CustomDrawerContent = (props) => {
   return (
     <DrawerContentScrollView {...props}>
@@ -175,6 +199,19 @@ const Main = () => {
           drawerIcon: ({ color }) => (
             <Icon 
               name='list'
+              type='font-awesome'
+              iconStyle={{ width: 24 }}
+              color={color}
+            />
+          ) }}
+        />
+        <Drawer.Screen
+          name="ReserveCampsite"
+          component={ReservationNavigator}
+          options={{ title: "Reserve Campsite",
+          drawerIcon: ({ color }) => (
+            <Icon 
+              name='tree'
               type='font-awesome'
               iconStyle={{ width: 24 }}
               color={color}
