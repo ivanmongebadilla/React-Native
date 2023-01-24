ee3a7e98fa3f8dc4fcf557e879880cda98fc402b
Ivan Monge <ivanmongebadilla@gmail.com>
2023-01-23 18:33:13 -0800

    Swipe and delete

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -16,6 +16,7 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
+import FavoritesScreen from './FavoritesScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -145,6 +146,29 @@ const ReservationNavigator = () => {
   );
 };
 
+const FavoritesNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={({ navigation }) => ({
+          title: 'Favorites Campsites',
+          headerLeft: () => (
+            <Icon 
+              name='heart'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+            />
+          )
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const CustomDrawerContent = (props) => {
   return (
     <DrawerContentScrollView {...props}>
@@ -212,6 +236,19 @@ const Main = () => {
           drawerIcon: ({ color }) => (
             <Icon 
               name='tree'
+              type='font-awesome'
+              iconStyle={{ width: 24 }}
+              color={color}
+            />
+          ) }}
+        />
+        <Drawer.Screen
+          name="Favorites"
+          component={FavoritesNavigator}
+          options={{ title: "My Favorites",
+          drawerIcon: ({ color }) => (
+            <Icon 
+              name='heart'
               type='font-awesome'
               iconStyle={{ width: 24 }}
               color={color}
